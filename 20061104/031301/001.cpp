4f8a9dab7d23142315a9e76191bc021267f246c3
shengrihui <1120058252@qq.com>
2022-02-21 16:31:26 +0800

    20220221clear

--- 20061104/031301/001.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define X 2+2
-int main()
-{
-	printf("%d\n",X/X);
-	printf("hello\n");
-	return 0;
-
-}
--- 20061104/031702/20051104031602.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a=12,b=5;
-
-	printf("a=%d%%,b=%d%%",a,b);
-	
-	return 0;
-}
